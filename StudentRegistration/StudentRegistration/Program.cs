ba1d04af38b7346dee9b78f01e0839b417c12922
Thomas Dahlinghaus <tdahlinghaus@maxtrain.com>
2021-03-15 19:26:30 -0400

    StudReg Practice Proj

---
+++ StudentRegistration/StudentRegistration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StudentRegistration {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Student Registration!");
+
+            Console.Write("Enter your first name: ");
+            String fName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            String lName = Console.ReadLine();
+            Console.Write("Enter your year of birth (YYYY format): ");
+            String bYear = Console.ReadLine();
+            String tempPassword = fName + "*" + bYear;
+            Console.WriteLine("Welcome  "+ fName + " " + lName + "!");
+            Console.WriteLine("Your registration is complete.");
+            Console.WriteLine("Your temporary password is: "+ tempPassword);
+                        
+                Console.WriteLine("Bye!");
+        }
+    }
+}
