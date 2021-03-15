3adeb15186c28cb691e2a6268ba5a97ef4be6282
Thomas Dahlinghaus <tdahlinghaus@maxtrain.com>
2021-03-15 19:28:34 -0400

    Some C# practice

---
+++ ControlStructures/ControlStructures/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ControlStructures {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Ch 4 Control Structures!");
+
+            int dow = 1;
+            switch (dow) {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                
+                default:
+                    Console.WriteLine("Invalid day.");
+                    break;
+            }
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine("loop #: " + i);
+
+
+
+            }
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
