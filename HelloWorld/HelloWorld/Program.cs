fc9eb05466f57e61feaa7d6706abfde63b23ed15
Thomas Dahlinghaus <tdahlinghaus@maxtrain.com>
2021-03-15 19:24:56 -0400

    Initial HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            int n1 = 6;
+            int n2 = 7;
+            
+            Console.WriteLine(n1 + n2);
+
+            Console.Write("Enter a whole number: ");
+            int 3 = Int32.Parse(Console.ReadLine());
+
+
+            Console.Write("Enter first name: ");
+            string fName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            String lName = Console.ReadLine();
+            String fullName = fName + lName;
+            Console.WriteLine(fullName);
+
+            String choice = "y";
+            while (choice.Equals("y")) {
+                Console.WriteLine("Enter color name:  (r/g/b) ");
+                String color = Console.ReadLine();
+                if (color=="r") {
+                    Console.WriteLine("Red was picked!");
+                }
+                if (color == "g") {
+                    Console.WriteLine("Green was picked!");
+                }
+                if (color == "b") {
+                    Console.WriteLine("Blue was picked!");
+                }
+                    Console.WriteLine("Continue (y/n)? ");
+                    choice = Console.ReadLine();
+            }
+            Console.WriteLine("Bye!");
+
+        }
+    }
+}
---
+++ MathApp/MathApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MathApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Math App!");
+            Console.WriteLine("Random #, dice roll...");
+            String choice = "y";
+            Random r = new Random();
+            while (choice == "y") {
+                int die = r.Next(1, 7);
+                Console.WriteLine(die);
+                Console.WriteLine("Again?");
+                choice = Console.ReadLine();
+
+            }
+            //random # between 20 and 50
+            int rand1 = r.Next(20, 51);
+
+            Console.WriteLine("round some numbers: ");
+            double d1 = 57.123456789;
+            Console.WriteLine(d1);
+            // format to 2 decimal places
+            Console.WriteLine(Math.Round(d1, 2));
+
+            // format currency
+            Console.WriteLine("Format a currency");
+            double price = 59.78;
+            Console.WriteLine(price.ToString("c"));
+
+            Console.WriteLine("Format a percent - 2 decimal places");
+            double percent = 3.45678;
+            Console.WriteLine(percent.ToString("f"));
+
+            Console.WriteLine("Bye!");
+        }
+    }
+}
